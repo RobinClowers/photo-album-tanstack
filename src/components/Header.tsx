9af7bdd5544326eda434e80197ec1bcb92ca2471
Robin Clowers <robin.clowers@gmail.com>
2026-05-05 21:41:58 -0700

    Home button works

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { Typography, AppBar, Toolbar, IconButton } from '@mui/material'
 import { Home } from '@mui/icons-material'
+import { Link } from '@tanstack/react-router'
 
 export function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" color="inherit" sx={{ mr: 2 }}>
+        <IconButton component={Link} to="/" edge="start" color="inherit" sx={{ mr: 2 }}>
           <Home />
         </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
